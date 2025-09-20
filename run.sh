9f5e6795702356e10b4ed4f95edf34a8311d07a8
xxxgqcoder <xxxgqcoder@gmail.com>
2025-09-20 11:23:17 +0800

    update

--- run.sh
+++ run.sh
@@ -5,8 +5,8 @@ cd $(dirname "$0")
 echo "working directory $(pwd)"
 
 file_path='/Users/xcoder/obsidian/Profession/PDF/Beyond Outlining- Heterogeneous Recursive Planning for Adaptive Long-form Writing with Language Models.pdf'
-temp_content_dir='./tmp/parsed_assets'final_md_file_save_dir
-final_md_file_save_dir='/Users/xcoder/obsidian/Profession/Processed PDF'
+temp_content_dir="./tmp/parsed_assets"
+final_md_file_save_dir="/Users/xcoder/obsidian/Profession/Processed PDF"
 
 
 python main.py \
